d000410be70d7c335e2200619f5f8492ec343fe1
Lisa Huang <lih226@lehigh.edu>
2024-11-28 03:16:38 -0500

    Edited server.js to be consist with get responses, changed the frontend to all fetch requests, comments working now

--- yourtube/src/App.js
+++ yourtube/src/App.js
@@ -1,62 +1,54 @@
 import React, { useEffect, useState } from 'react';
 import Post from './components/Post.js';
-// import axios from 'axios';
+import axios from 'axios';
 
-// const ipAddress = 'localhost';
+const PORT = '5000';
 
 export default function App() {
-  let posts = [{"p_id":1,"u_id":1,"title":"https://www.youtube.com/watch?v=jfKfPfyJRdk","body":"A classic","likes":20,"dislikes":2},
-    {"p_id":2,"u_id":1,"title":"https://www.youtube.com/watch?v=TyUA1OmXMXA","body":"Learned a lot","likes":100,"dislikes":4},
-    {"p_id":3,"u_id":4,"title":"https://www.youtube.com/watch?v=ZAqIoDhornk","body":"Physics!!!!","likes":1,"dislikes":0},
-    {"p_id":4,"u_id":2,"title":"https://www.youtube.com/watch?v=vr5dCRHAgb0","body":"sooo relaxing","likes":20,"dislikes":2},
-    {"p_id":5,"u_id":5,"title":"https://www.youtube.com/watch?v=xvFZjo5PgG0","body":"It is soooo good!!","likes":2,"dislikes":20},
-    {"p_id":6,"u_id":5,"title":"https://www.youtube.com/watch?v=xvFZjo5PgG0","body":"It is soooo good!!!!!!","likes":2,"dislikes":100}];
-  
-  let users = [{"u_id":1,"f_name":"Mustang","l_name":"John","is_admin":false},
-    {"u_id":2,"f_name":"John","l_name":"Phillips","is_admin":true},
-    {"u_id":3,"f_name":"Katusha","l_name":"Vasilischev","is_admin":true},
-    {"u_id":4,"f_name":"Hal","l_name":"Gloster","is_admin":false},
-    {"u_id":5,"f_name":"Henry","l_name":"Schumacher","is_admin":false}];
-  
-  let comments = [{"c_id":1,"u_id":2,"p_id":1,"body":"I AGREE!!!","likes":100,"dislikes":1},
-    {"c_id":2,"u_id":5,"p_id":1,"body":"I Watch This Like Everyday!!!","likes":500,"dislikes":10},
-    {"c_id":3,"u_id":1,"p_id":6,"body":"Please remove for spamming","likes":200,"dislikes":0},
-    {"c_id":4,"u_id":1,"p_id":6,"body":"AHHHHHHH","likes":100,"dislikes":1}];
-  
-  // const [data, setData] = useState(null);
-  // const [error, setError] = useState(null);
-  // const [refreshing, setRefreshing] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState("");
+  const [refreshing, setRefreshing] = useState(false);
 
-  // useEffect(() => {
-  //   axios.get('http://' + ipAddress + ':3000/posts')
-  //     .then(response => {
-  //       setData(response.data);
-  //     })
-  //     .catch(error => {
-  //       setError(error.message);
-  //     });
-  // });
+  useEffect(() => {
+    axios.get('http://localhost:' + PORT + '/api/posts')
+      .then(response => {
+        setPosts(response.data);
+      })
+      .catch(err => {
+        setError(err.message);
+        console.log(error);
+      });
+    axios.get('http://localhost:' + PORT + '/api/users')
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(err => {
+        setError(err.message);
+        console.log(error);
+      });
+  }, [PORT]);
 
-  // function onRefresh(){
-  //   setRefreshing(true);
-  //   axios.get('http://' + ipAddress + ':3000/posts')
-  //     .then(response => {
-  //       setData(response.data);
-  //       setRefreshing(false);
-  //     })
-  //     .catch(error => {
-  //       setError(error.message);
-  //       setRefreshing(false);
-  //     });
-  // };
+  function onRefresh(){
+    setRefreshing(true);
+    axios.get('http://localhost:' + PORT + '/api/users')
+      .then(response => {
+        setPosts(response.data);
+        setRefreshing(false);
+      })
+      .catch(error => {
+        setError(error.message);
+        setRefreshing(false);
+      });
+  };
 
   return (
     <div style={styles.container}>
-      {posts.map(e => 
-        <div>
-          <Post videoLink={e.title} body={e.body} name={users.filter(user => user.u_id === e.u_id)[0].f_name} likes={e.likes} dislikes={e.dislikes} comments={comments.filter(comment => comment.p_id === e.p_id)}/>
+      {(posts.length > 0 && users.length > 0) ? (posts.map(e =>
+        <div key={e.p_id}>
+          <Post videoLink={e.title} body={e.body} name={users.filter(user => user.u_id === e.u_id)[0].f_name + " " + users.filter(user => user.u_id === e.u_id)[0].l_name} likes={e.likes} dislikes={e.dislikes} p_id={e.p_id} PORT={PORT} />
         </div>
-      )}
+      )) : (<p>Fetching data...</p>)}
     </div>
   );
 }
@@ -65,6 +57,5 @@ const styles = {
   container: {
     flex: 1,
     alignItems: "center",
-    backgroundColor: '#FEFAFF',
   }
 };
--- yourtube/src/components/Post.js
+++ yourtube/src/components/Post.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PropTypes from 'prop-types';
 import YouTube from "react-youtube";
+import axios from "axios";
 
 //Reference for embedding YouTube video
 //Reference: https://medium.com/@otooker/embedding-a-youtube-video-in-react-9be0040b050d
-const Post = ({videoLink, body, name, likes, dislikes, comments}) => {
+const Post = ({videoLink, body, name, likes, dislikes, p_id, PORT}) => {
     //Used the URLSearchParams interface to get the videoId: https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
     const searchParam = new URLSearchParams(new URL(videoLink).search);
     const videoId = searchParam.get("v");
+    const [comments, setComments] = useState([]);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        axios.get('http://localhost:' + PORT + '/api/posts/' + p_id)
+          .then(response => {
+            setComments(response.data.comments);
+            console.log(response.data);
+          })
+          .catch(err => {
+            setError(err.message);
+            console.log(err.message);
+          });
+      }, [PORT]);
 
     const options = {
         height: "195",
@@ -26,14 +41,18 @@ const Post = ({videoLink, body, name, likes, dislikes, comments}) => {
         <div>
             <h2>{name}: {body}</h2>
             <YouTube videoId={videoId} opts={options} onReady={_onReady} id="video"/>
-            <h4>Likes: {likes}   Dislikes: {dislikes}</h4>
+            <p>Likes: {likes}   Dislikes: {dislikes}</p>
             <h3>Comments: </h3>
-            {comments.map(e => {
-                <div>
-                    <h4>{e.u_id}: {e.body}</h4>
-                    <h5>Likes: {e.likes}   Dislikes: {e.dislikes}</h5>
-                </div>
-            })}
+            {(comments.length > 0) ? (
+                comments.map(e => 
+                    <div key={e.c_id} style={styles.comments}>
+                        <p>{e.u_id}: {e.body}</p>
+                        <p>Likes: {e.likes}   Dislikes: {e.dislikes}</p>
+                    </div>
+                )
+            ) : (
+                <p></p>
+            )}
         </div>
     )
 }
@@ -44,7 +63,14 @@ Post.propTypes = {
     name: PropTypes.string.isRequired,
     likes: PropTypes.number.isRequired,
     dislikes: PropTypes.number.isRequired,
-    comments: PropTypes.array.isRequired,
+    p_id: PropTypes.number.isRequired,
+    PORT: PropTypes.string.isRequired,
   }
+
+const styles = {
+    comments: {
+        marginLeft: 20,
+    }
+};
 
 export default Post;
